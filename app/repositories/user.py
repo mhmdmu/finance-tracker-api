ae034bfd02ab542dcf55eda982455de1d3facdb9
Muhammad Mustafa <momustafa683@gmail.com>
2026-04-26 02:04:52 +0300

    feat: implement users repository layer

---
+++ app/repositories/user.py
@@ -0,0 +1,23 @@
+from database import pool
+
+
+async def get_user_by_username(username: str):
+    query = """
+    SELECT *
+    FROM users
+    WHERE username = $1
+    """
+
+    async with pool.acquire() as conn:
+        return await conn.fetchrow(query, username)
+
+
+async def create_user(username: str, password: str):
+    query = """
+    INSERT INTO users(username, password)
+    VALUES($1, $2)
+    RETURNING id, username
+    """
+
+    async with pool.acquire() as conn:
+        return await conn.fetchrow(query, username, password)
