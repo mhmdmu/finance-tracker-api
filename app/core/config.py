f9d8e620bc8f9a7b67e34895e1bfecf8fa810195
Muhammad Mustafa <momustafa683@gmail.com>
2026-04-26 04:10:28 +0300

    fix: configure settings to load from .env

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -6,5 +6,8 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     EXPIRE_MINUTES: int
+
+    model_config = SettingsConfigDict(env_file=".env")
+
 
 settings = Settings()
