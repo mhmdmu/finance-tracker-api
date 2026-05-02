251767750cf055dfc04c5dcf9216e88a2b7d5527
Muhammad Mustafa <momustafa683@gmail.com>
2026-05-02 13:12:17 +0300

    fix: handle account not found when fetching from DB

--- app/account/service.py
+++ app/account/service.py
@@ -11,12 +11,22 @@ async def get_all_accounts(user_id: int, conn: Connection):
 
 
 async def get_account(id: int, user_id: int, conn: Connection):
-    return dict(await repo.get_account_by_id(id, user_id, conn))
+    account = await repo.get_account_by_id(id, user_id, conn)
+
+    if account is None:
+        raise AccountNotFound(id)
+
+    return dict(account)
 
 
 async def create_account(user_id: int, type: str, name: str, conn: Connection):
     try:
-        return dict(await repo.create_account(user_id, type.lower(), name, conn))
+        account = await repo.create_account(user_id, type.lower(), name, conn)
+
+        if account is None:
+            raise AccountNotFound(id)
+
+        return dict(account)
     except InvalidTextRepresentationError:
         raise InvalidAccountType()
 
@@ -27,7 +37,12 @@ async def modify_account(
     type: str | None = None,
     name: str | None = None,
 ):
-    return dict(await repo.modify(id, type, name, conn))
+    account = await repo.modify(id, type, name, conn)
+
+    if account is None:
+        raise AccountNotFound(id)
+
+    return dict(account)
 
 
 async def delete_account(id: int, conn: Connection):
