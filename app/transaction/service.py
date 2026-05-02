6018ba3382c5d959a833d111273d7dcb8c1775d5
Muhammad Mustafa <momustafa683@gmail.com>
2026-05-02 22:45:52 +0300

    chore: remove print statement from transaction service

--- app/transaction/service.py
+++ app/transaction/service.py
@@ -55,7 +55,6 @@ async def create_transaction(
 
 async def delete_transaction(acc_id: int, trans_id: int, conn: Connection):
     deleted = await repo.delete_transaction(acc_id, trans_id, conn)
-    print(f"{deleted=}")
 
     if deleted is None:
         raise TransactionNotFound(trans_id)
